4a1091ac7983ae7f995a5ae102be3fd254af1c09
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-03-09 16:49:27 -0400

    some minimal work on powerups

--- SEproject/Assets/script/DeerPlayer.cs
+++ SEproject/Assets/script/DeerPlayer.cs
@@ -15,4 +15,28 @@ public class Deer : MonoBehaviour
     {
         
     }
+
+    public void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        Powerup.PowerupType? powerupType = collision.gameObject.GetComponent<Powerup>()?.powerupType;
+
+        if (powerupType == null)
+        {
+            return;
+        }
+
+        switch (powerupType)
+        {
+            case Powerup.PowerupType.SPEED:
+                Debug.Log("Speed powerup collected");
+
+                break;
+            case Powerup.PowerupType.JUMP:
+                Debug.Log("Jump powerup collected");
+                break;
+            case Powerup.PowerupType.INVINCIBILITY:
+                Debug.Log("Invincibility powerup collected");
+                break;
+        }
+    }
 }
